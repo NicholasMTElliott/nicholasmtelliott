e087c356fb68bec32974bce844f1cf48067bb1cb
Nicholas Elliott <nicholasmtelliott@gmail.com>
2020-11-15 17:29:06 -0500

    Portfolio example

--- components/layout/LayoutWithHeader.tsx
+++ components/layout/LayoutWithHeader.tsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { Link } from 'gatsby';
 import './LayoutWithHeader.scss';
 
 export default function  LayoutWithHeader(props:{children:any})
 {
-	return <div className='LayoutWithHeader'>
+	return <header className='LayoutWithHeader'>
 		<div className='LayoutHeaderBar'>
 			<img className='LayoutAvatar' src='https://gravatar.com/avatar/468788fd550c4128b0c250566ea8729f?s=128' />
-			<div>
-			Name * Title * Location <br/>
-			menubar
+			<div className='LayoutSideMenu'>
+				<summary><h1>Nicholas M.T. Elliott</h1>  <h2>Managing Director, Solution Architect</h2> <h2>Columbia, MD, USA</h2></summary>
+				<nav><Link to='/'>Home</Link> &#11044; <Link to='/portfolio'>Portfolio</Link> &#11044;  <Link to='/about'>About</Link></nav>
 			</div>
 		</div>
 		<div className='LayoutContext'>
 			{props.children}
 		</div>
-	</div>;
+	</header>;
 }
