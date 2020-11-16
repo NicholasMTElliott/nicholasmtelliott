ae0ba91fe442001639993723b83851e8c6580767
Nicholas Elliott <nicholasmtelliott@gmail.com>
2020-11-16 10:33:12 -0500

    Revised front-page

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,13 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-netlify-cms', 'gatsby-transformer-remark',
+  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-netlify-cms', 'gatsby-transformer-remark', 
+  {
+    resolve: `gatsby-plugin-typography`,
+    options: {
+      pathToConfigModule: `src/utils/typography`,
+    },
+  },
   {
     resolve: `gatsby-source-filesystem`,
     options: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,37 @@
 import { Link, PageProps } from "gatsby"
 import React from "react"
-import LayoutWithHeader from "../../components/layout/LayoutWithHeader";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as icons from '../utils/icons';
 import './index.scss';
 
 export default function Home(props:PageProps) {
-  return <LayoutWithHeader>
-      Hello world! {props.path} 
-      <Link to='/about' >About us</Link>
-  </LayoutWithHeader>;
+  return <div className='root'>
+      <header>
+        <aside><h3>Nicholas M.T. Elliott</h3></aside>
+        <nav>
+          <Link to='/me'>About Me</Link>
+          <Link to='/portfolio'>Portfolio</Link>
+          <Link to='/site'>This Site</Link>
+        </nav>
+      </header>
+      <main>
+			  <div className='above'>
+          <img className='LayoutAvatar' src='https://gravatar.com/avatar/468788fd550c4128b0c250566ea8729f?s=64' />
+          <h1>Nicholas M.T. Elliott</h1>
+        </div>
+
+        <div className='below'>
+          <h3>Managing Director &#11044; Solution Architect</h3>
+          <h5>Skyward App Company &#11044; Columbia, Maryland</h5>
+          <div className='social'>
+            <a href='https://twitter.com/lightsidenick'><FontAwesomeIcon icon={icons.faTwitter}/></a>
+            <a href='https://www.linkedin.com/in/nicholasmtelliott/'><FontAwesomeIcon icon={icons.faLinkedin}/></a>
+            <a href='https://github.com/NicholasMTElliott'><FontAwesomeIcon icon={icons.faGithub}/></a>
+          </div>
+        </div>
+      </main>
+      <footer>
+
+      </footer>
+  </div>;
 }
